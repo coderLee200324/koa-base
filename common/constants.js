933463f43f2697d352b439a159d7b2c4dfe50dec
lwb807 <895180424@qq.com>
2020-07-29 09:35:56 +0800

    常量文件夹

---
+++ common/constants.js
@@ -0,0 +1,12 @@
+// 用户所需要的常量
+const API_SUCC = 0; // 成功
+const API_NO_PERMISSION = 403; // 无权限
+const API_RES_NOT_EXIST = 404; // 资源不存在
+const API_INTERNAL_ERROR = 500; // 未确定内部错误
+
+module.exports = {
+  API_SUCC,
+  API_NO_PERMISSION,
+  API_RES_NOT_EXIST,
+  API_INTERNAL_ERROR,
+};
